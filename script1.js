b566412afc9c88e71d8a7c3189ac96302e572544
pratikhire95@gmail.com <pratikhire95@gmail.com>
2020-05-21 02:35:26 +0530

    adding starting project

---
+++ script1.js
@@ -0,0 +1,14 @@
+const obj{
+    const player='bobby';
+    const experience=100;
+    let wizardLevel=false
+}
+
+const{player,experince}=obj;
+
+let{wizardLevel}=obj;
+
+ player='pratik';
+ wizardLevel=true;
+
+ 
